873adcfbc5c8eb6c5ba5b6983c0b163b99feda60
AppolinFotso <appolin.s.fotso@gmail.com>
2023-09-25 22:10:00 +0100

    Loadingsuggestions sentence

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,6 +9,11 @@ import NoneOfTheSuggestions from "./NoneOfTheSuggestions";
 
 export function Home() {
 	const [randomText, setRandomText] = useState("Loading...");
+	const [suggestionsText, setSuggestionsText] = useState([
+		"Loading...",
+		"Loading...",
+		"Loading...",
+	]);
 //
 	const getRandomIndex = (length) => {
 		return Math.floor(Math.random() * length);
@@ -24,6 +29,18 @@ export function Home() {
 		};
 		loadRandomSentenceFromFile();
 	}, []);
+	//
+	useEffect(() => {
+		const getSuggestionsFromApi = async (text) => {
+			const response = await fetch(
+				`https://angocair.garg.ed.ac.uk/best/?text=${encodeURIComponent(text)}`
+			);
+			const data = await response.json();
+			setSuggestionsText(data.data);
+		};
+		getSuggestionsFromApi(randomText);
+	}, [randomText]);
+
 	
 	return (
 		<main role="main">
