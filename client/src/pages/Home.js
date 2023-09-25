f1050c5b54b58ce0e609b6bda1174b826264fe6c
AppolinFotso <appolin.s.fotso@gmail.com>
2023-09-25 21:56:12 +0100

    Removed unused data from Home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,8 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
-import logo from "./logo.svg";
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
 
 	useEffect(() => {
 		fetch("/api")
@@ -26,16 +24,7 @@ export function Home() {
 	return (
 		<main role="main">
 			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
-				<Link to="/about/this/site">About</Link>
+				
 			</div>
 		</main>
 	);
