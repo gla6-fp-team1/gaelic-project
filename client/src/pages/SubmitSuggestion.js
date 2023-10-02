190e2ed91698421c5cda89e2cfd396cf6ae91329
AppolinFotso <appolin.s.fotso@gmail.com>
2023-10-02 10:25:03 +0100

    Added fetch with POST request to SubmitSuggestion component to send data to the server

--- client/src/pages/SubmitSuggestion.js
+++ client/src/pages/SubmitSuggestion.js
@@ -1,8 +1,23 @@
-const SubmitSuggestion = () => {
+const SubmitSuggestion = (props) => {
 	return (
 		<div className="flex-end">
 			<button
 				className="width submit"
+				onClick={() => {
+					const data = new URLSearchParams();
+					data.append("sentence", props.randomText);
+					data.append("suggestions", props.suggestionsText);
+					data.append("selectedSuggestion", props.selectedSuggestion);
+
+					fetch("http://localhost:3000/api/save-suggestions", {
+						method: "POST",
+						body: data,
+					})
+						.then((res) => res.json())
+						.then((data) => {
+							console.log(data.message);
+						});
+				}}
 			>
 				Submit Suggestion
 			</button>
