3169e57056da74b3c93ae13c8b3ac30ebffa2de2
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-10-07 11:43:06 +0100

    add routes for the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/login" element={<Login />} />
-	</Routes>
-);
+const App = () => {
+	const user = false;
+	return (
+		<Routes>
+			<Route path="/" element={!user ? <Navigate to="/login" /> : <Home /> } />
+			<Route path="/about/this/site" element={<About />} />
+			<Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+		</Routes>
+	);
+};
 
 export default App;
-
