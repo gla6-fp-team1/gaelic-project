8dbebb2e4ae22304484b7efb0d22581fa44ab8ab
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-10-07 13:11:42 +0100

    replaced <a> tags with Link components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,13 +11,11 @@ const Navbar = () => {
 			</span>
 			<ul>
 				<li>
-					<a href="/">Home</a>
+                    <Link to="/">Home
+                    </Link>
 				</li>
 				<li>
-					<a href="/about">About</a>
-				</li>
-				<li>
-					<a href="/contact">Contact</a>
+					<Link to="/about/this/site">About</Link>
 				</li>
 			</ul>
 		</nav>
