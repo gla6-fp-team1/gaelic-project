28092a36ee6385bc9d8827e7eafa3e47799b2982
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-10-08 12:12:44 +0100

    removed passport.js and moved content to auth.js

--- server/auth/routes/auth.js
+++ server/auth/routes/auth.js
@@ -1,7 +1,40 @@
+require("dotenv");
 const router = require("express").Router();
 const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const CLIENT_URL = "http://localhost:3000/";
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://www.localhost:3000/auth/google/callback",
+		},
+		function (accessToken, refreshToken, profile, done) {
+			done(null, profile);
+
+			/* Find or create user in database
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+    */
+		}
+	)
+);
+
+
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
 
 router.get("/login/failed", (req, res) => {
 	res.status(401).json({
@@ -15,15 +48,15 @@ router.get("/login/success", (req, res) => {
 		res.status(200).json({
 			success: true,
 			message: "User has been successfully authenticated.",
-            user: req.user,
-            cookies: req.cookies,
+			user: req.user,
+			cookies: req.cookies,
 		});
 	}
 });
 
 router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect(CLIENT_URL);
+	req.logout();
+	res.redirect(CLIENT_URL);
 });
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
