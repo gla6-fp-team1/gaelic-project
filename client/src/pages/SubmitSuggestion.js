8fa17e9b5c18c0158f4e552c20b835f595535c0f
AppolinFotso <appolin.s.fotso@gmail.com>
2023-10-09 15:53:44 +0100

    Refactored a new submitButton function and passed it as props to PopUpAlert component

--- client/src/pages/SubmitSuggestion.js
+++ client/src/pages/SubmitSuggestion.js
@@ -1,30 +1,31 @@
 import PopUpAlert from "../components/PopUpAlert";
 
 const SubmitSuggestion = (props) => {
+	const submitButton = () => {
+		const data = new URLSearchParams();
+		data.append("sentence", props.randomText);
+		data.append("suggestions", props.suggestionsText);
+		data.append("selectedSuggestion", props.selectedSuggestion);
+
+		fetch("/api/save-suggestions", {
+			method: "POST",
+			body: data,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data.message);
+			});
+		let randomNumber = Math.random() * 1000;
+		props.setNextOriginalText(randomNumber);
+	};
 	return (
 		<div className="flex-end">
 			<PopUpAlert text={"Submit Suggestion"}
 				className="width submit"
-				onClick={() => {
-					const data = new URLSearchParams();
-					data.append("sentence", props.randomText);
-					data.append("suggestions", props.suggestionsText);
-					data.append("selectedSuggestion", props.selectedSuggestion);
-
-					fetch("/api/save-suggestions", {
-						method: "POST",
-						body: data,
-						headers: {
-							"Access-Control-Allow-Origin" : "*",
-						},
-					})
-						.then((res) => res.json())
-						.then((data) => {
-							console.log(data.message);
-						});
-					let randomNumber = Math.random() * 1000;
-					props.setNextOriginalText(randomNumber);
-				}}
+				submitButton={submitButton}
 			/>
 		</div>
 	);
