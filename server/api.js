0a2930f25866c0dbcac0305a332df78855ef7494
AppolinFotso <appolin.s.fotso@gmail.com>
2023-10-11 17:03:03 +0100

    Removed the hardcoded text on the endpoint /api and added a query to fetch randomtext from the database

--- server/api.js
+++ server/api.js
@@ -6,25 +6,22 @@ const fs = require("fs/promises");
 
 const router = Router();
 
-router.get("/", (_, res) => {
+router.get("/", async(_, res) => {
 	logger.debug("Welcoming everyone...");
-	const text = [
-		"Bhiodh iad a' dèanamh móran uisge-bheatha bho chionn fhada ro n a latha againn.",
-		"Tha iad ris an Gearrloch fhathast.",
-		"Well, chuala mi gun robh iad ris an Gearrloch a sin.",
-		"Bha iad a' faicinn bòcan shios ann an Arasaig.",
-		"agus 's e a'chiad rud a bha iad a' faicinn dhe'n rud colainn gun cheann a' falbh feadh siod.",
-		"agus bha bodach a' fuireach ann a' sabhall.",
-		"Chuir iad duine ann a' sabhall a dh' fhuireach oidhche.",
-		"agus chunnaic e am bòcan a bha seo agus an bòcan bhitheadh aige.",
-		"agus tha obair uamhasach aige mun d' fhuair e am bòcan a chumail bhuaidh.",
-		"agus bha coltas air gun deànadh am bòcan an gróthach air.",
-		"Cha robh buille a bhuaileadh e air nach robh e a' smaoineach' gur h- ann air póca cloìmh leis cho bog 's a bha a' bhuille is cha robh i a' dèanadh feum.",
-	];
+
 	const getRandomIndex = (length) => {
 		return Math.floor(Math.random() * length);
 	};
-	res.json(text[getRandomIndex(text.length)]);
+	// Select sentences from sentences table
+	const sentences = await db.query(
+		"SELECT sentence FROM sentences WHERE count >= 1"
+	);
+	const randomSentences = [];
+	for (let sentence of sentences.rows) {
+		randomSentences.push(sentence.sentence);
+	}
+	// const randomSentence = sentences.rows[getRandomIndex(sentences.rows.length)];
+	res.json(randomSentences[getRandomIndex(randomSentences.length)]);
 });
 
 router.post("/save-suggestions", async (req, res) => {
