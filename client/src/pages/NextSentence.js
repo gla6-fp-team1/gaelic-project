0e6d6261109875baa9be6955fd845ec0b6354468
nasir ali <a.nasirali@outlook.com>
2023-10-13 14:00:55 +0100

    working on download button

--- client/src/pages/NextSentence.js
+++ client/src/pages/NextSentence.js
@@ -1,37 +1,31 @@
-
 const exportGaelicData = async (data) => {
 	try {
- const jsonData = JSON.stringify(data, null, 2);
- const blob = new Blob([jsonData], { type: "application/json" });
- const link = document.createElement("a");
- link.href = URL.createObjectURL(blob);
- link.download = "exported_data.json";
-document.body.appendChild(link);
-link.click();
-setTimeout(() => {
-		document.body.removeChild(link);
-}, 0);
+		const jsonData = JSON.stringify(data, null, 2);
+		const blob = new Blob([jsonData], { type: "application/json" });
+		const link = document.createElement("a");
+		link.href = URL.createObjectURL(blob);
+		link.download = "exported_data.json";
+		document.body.appendChild(link);
+		link.click();
+		setTimeout(() => {
+			document.body.removeChild(link);
+		}, 0);
 	} catch (error) {
- console.error("Error exporting data:", error);
-
+		console.error("Error exporting data:", error);
 	}
-  };
-
-
-
-
+};
 
 const NextSentence = (props) => {
 	const handleExportGaelicData = async () => {
-	try {
- const response = await fetch("/api/exportGaelicData");
- const data = await response.json();
- console.log(data);
-exportGaelicData(data);
+		try {
+			const response = await fetch("/api/exportGaelicData");
+			const data = await response.json();
+			console.log(data);
+			exportGaelicData(data);
 		} catch (error) {
- console.error("Error fetching Gaelic data:", error);
-	}
-};
+			console.error("Error fetching Gaelic data:", error);
+		}
+	};
 
 	return (
 		<div className="nextExportButton">
@@ -43,9 +37,12 @@ exportGaelicData(data);
 				}}
 			>
 				Next
-			</button><button onClick={handleExportGaelicData}>ExportGaelicData</button></div>
+			</button>
+			<button onClick={handleExportGaelicData}>ExportGaelicData</button>
+		</div>
+
+		//  trying to fix this so its smaller and does not affect bandwidth
 	);
 };
 
 export default NextSentence;
-
